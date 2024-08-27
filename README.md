48fafb10219ab4a542e10ddd1813e9fe8b7fd1f7
Kkih <57308042+D071N@users.noreply.github.com>
2024-08-28 01:58:45 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+/usr/bin/python3 -m pip install --upgrade pip && pip3 uninstall telegram && pip3 uninstall telegram-bot python-telegram-bot && pip3 install -r requirements.txt && cd hulkserver && pip3 install -r requirements.txt 
+
+
+connact telegram bot make a .env file with a TELEGRAM_TOKEN variable with your telegram token key, and make a NUM variable with the name of the bot.
---
+++ server.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template
+import subprocess
+import os 
+import signal
+
+app = Flask(__name__)
+
+bot_process = subprocess.Popen(['python3' , 'bot.py'])
+
+
+def kill_subprocesses():
+    current_pid = os.getpid()
+    cmd = f'pgrep -P {current_pid}'
+    pids = subprocess.check_output(cmd, shell=True).decode().split()
+    for pid in pids:
+        try:
+            os.kill(int(pid), signal.SIGTERM)
+        except OSError:
+            pass
+
+
+
+
+@app.route("/")
+def page_web_de_mort():
+    bot_process
+    return "<html><body><img src='https://c.tenor.com/e2VK6sB1TX0AAAAd/tenor.gif'></body></html>"
+    #page_web_de_mort()
+    
+@app.route("/kill")
+def killswitch():
+    kill_subprocesses()
+    p = subprocess.Popen(f'refresh', stdout=subprocess.PIPE, shell=True)
+    output, error = p.communicate()
+    bot_process
+    return "<html><body><img src='https://c.tenor.com/e2VK6sB1TX0AAAAd/tenor.gif'></body></html>"
+    #page_web_de_mort()    
+    
+
+app.run()
